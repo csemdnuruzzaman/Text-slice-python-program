7c0d598223a28bcf96edd04c005360236aaf81c4
csemdnuruzzaman <nuruzzamanpolytechnic@gmail.com>
2023-03-06 16:39:28 +0600

    first commit

---
+++ slice any text.py
@@ -0,0 +1,7 @@
+str = "Hello Nuruzzaman"
+print(str)#prints complete string
+print(str[0])#prints first characters of the string
+print(str[2:5])#prints characters starting from 3rd to 5th
+print(str[2:])#prints string starting from 3rd character
+print(str*2)#prints string two times
+print(str+"Welcome")#prints concatenated string
